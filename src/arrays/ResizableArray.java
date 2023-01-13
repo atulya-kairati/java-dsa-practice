764089daf704d188acfc3983ae8970d6ae84d385
Atulya Kairati <atulya.kairati@onmobile.com>
2023-01-13 11:31:44 +0530

    resizable arrays

---
+++ src/arrays/ResizableArray.java
@@ -0,0 +1,41 @@
+package arrays;
+
+public class ResizableArray{
+
+    int size, capacity;
+    int[] array;
+
+    ResizableArray(){
+        this.capacity = 2;
+        this.size = 0;
+
+        array = new int[capacity];
+    }
+
+    void push(int e) {
+        if(size < capacity){
+            array[size++] = e;
+            return;
+        }
+
+        capacity *= 2;
+        int[] newArray = new int[capacity];
+
+        for(int i = 0; i < size; i++){
+            newArray[i] = array[i];
+        }
+        newArray[size++] = e;
+
+        this.array = newArray;
+    }
+
+    @Override
+    public String toString() {
+        String res = "ResizableArray[ ";
+
+        for(int i = 0; i < size; i++){
+            res += array[i] + ", ";
+        }
+        return res + "]";
+    }
+}
