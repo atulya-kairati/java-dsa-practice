dd03ce575474236d26cba6cf7c55f5e3ec54da6a
Atulya Kairati <atulyakairati@gmail.com>
2023-01-17 09:06:19 +0530

    single number

---
+++ src/arrays/SingleNumber.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class SingleNumber {
+    public static int solution(int[] arr) {
+        int ans = 0;
+
+        for (int e : arr) {
+            ans ^= e;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 2, 1, 3, 3, 4, 2}));
+    }
+}
